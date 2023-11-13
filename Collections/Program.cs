4dacecb65cc4db1c21b79c20991a83ea4fbcbbc2
hasankesil <hasankesil@hotmail.com>
2023-11-13 09:58:57 +0300

    Lists

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);  
+
+            List<string> isimler2 = new List<string> { "engin", "halil", "murat", "kerem"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+        }
+    }
+}
